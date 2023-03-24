0ffd5ee6ede4a3187a17252ba3e161b05f9c6b55
vitodepi16 <118888625+vitodepi16@users.noreply.github.com>
2023-03-24 18:41:58 +0100

    finish select

--- js/script.js
+++ js/script.js
@@ -1,124 +1,123 @@
 const icons = [
-    {
-        name: 'cat',
-        prefix: 'fa-',
-        type: 'animal',
-        family: 'solid',
-        color: 'orange'
-    },
-    {
-        name: 'crow',
-        prefix: 'fa-',
-        type: 'animal',
-        family: 'solid',
-        color: 'orange'
-    },
-    {
-        name: 'dog',
-        prefix: 'fa-',
-        type: 'animal',
-        family: 'solid',
-        color: 'orange'
-    },
-    {
-        name: 'dove',
-        prefix: 'fa-',
-        type: 'animal',
-        family: 'solid',
-        color: 'orange'
-    },
-    {
-        name: 'dragon',
-        prefix: 'fa-',
-        type: 'animal',
-        family: 'solid',
-        color: 'orange'
-    },
-    {
-        name: 'horse',
-        prefix: 'fa-',
-        type: 'animal',
-        family: 'solid',
-        color: 'orange'
-    },
-    {
-        name: 'hippo',
-        prefix: 'fa-',
-        type: 'animal',
-        family: 'solid',
-        color: 'orange'
-    },
-    {
-        name: 'fish',
-        prefix: 'fa-',
-        type: 'animal',
-        family: 'solid',
-        color: 'orange'
-    },
-    {
-        name: 'carrot',
-        prefix: 'fa-',
-        type: 'vegetable',
-        family: 'solid',
-        color: 'green'
-    },
-    {
-        name: 'apple-alt',
-        prefix: 'fa-',
-        type: 'vegetable',
-        family: 'solid',
-        color: 'green'
-    },
-    {
-        name: 'lemon',
-        prefix: 'fa-',
-        type: 'vegetable',
-        family: 'solid',
-        color: 'green'
-    },
-    {
-        name: 'pepper-hot',
-        prefix: 'fa-',
-        type: 'vegetable',
-        family: 'solid',
-        color: 'green'
-    },
-    {
-        name: 'user-astronaut',
-        prefix: 'fa-',
-        type: 'user',
-        family: 'solid',
-        color: 'blue'
-    },
-    {
-        name: 'user-graduate',
-        prefix: 'fa-',
-        type: 'user',
-        family: 'solid',
-        color: 'blue'
-    },
-    {
-        name: 'user-ninja',
-        prefix: 'fa-',
-        type: 'user',
-        family: 'solid',
-        color: 'blue'
-    },
-    {
-        name: 'user-secret',
-        prefix: 'fa-',
-        type: 'user',
-        family: 'solid',
-        color: 'blue'
-    }
+  {
+    name: "cat",
+    prefix: "fa-",
+    type: "animal",
+    family: "solid",
+    color: "orange",
+  },
+  {
+    name: "crow",
+    prefix: "fa-",
+    type: "animal",
+    family: "solid",
+    color: "orange",
+  },
+  {
+    name: "dog",
+    prefix: "fa-",
+    type: "animal",
+    family: "solid",
+    color: "orange",
+  },
+  {
+    name: "dove",
+    prefix: "fa-",
+    type: "animal",
+    family: "solid",
+    color: "orange",
+  },
+  {
+    name: "dragon",
+    prefix: "fa-",
+    type: "animal",
+    family: "solid",
+    color: "orange",
+  },
+  {
+    name: "horse",
+    prefix: "fa-",
+    type: "animal",
+    family: "solid",
+    color: "orange",
+  },
+  {
+    name: "hippo",
+    prefix: "fa-",
+    type: "animal",
+    family: "solid",
+    color: "orange",
+  },
+  {
+    name: "fish",
+    prefix: "fa-",
+    type: "animal",
+    family: "solid",
+    color: "orange",
+  },
+  {
+    name: "carrot",
+    prefix: "fa-",
+    type: "vegetable",
+    family: "solid",
+    color: "green",
+  },
+  {
+    name: "apple-alt",
+    prefix: "fa-",
+    type: "vegetable",
+    family: "solid",
+    color: "green",
+  },
+  {
+    name: "lemon",
+    prefix: "fa-",
+    type: "vegetable",
+    family: "solid",
+    color: "green",
+  },
+  {
+    name: "pepper-hot",
+    prefix: "fa-",
+    type: "vegetable",
+    family: "solid",
+    color: "green",
+  },
+  {
+    name: "user-astronaut",
+    prefix: "fa-",
+    type: "user",
+    family: "solid",
+    color: "blue",
+  },
+  {
+    name: "user-graduate",
+    prefix: "fa-",
+    type: "user",
+    family: "solid",
+    color: "blue",
+  },
+  {
+    name: "user-ninja",
+    prefix: "fa-",
+    type: "user",
+    family: "solid",
+    color: "blue",
+  },
+  {
+    name: "user-secret",
+    prefix: "fa-",
+    type: "user",
+    family: "solid",
+    color: "blue",
+  },
 ];
-const row = document.querySelector('.row');
-let rowHtml = '';
+const row = document.querySelector(".row");
+let rowHtml = "";
 
-
-
-function drawCard(icons){
-    icons.forEach((icons) => {
+function drawCard(icon) {
+  let rowHtml = "";
+  icon.forEach(function (icons) {
     let cardTpl = `<div class="col-5 col-lg-3 col-sm-2">
     <div class="card m-2  d-flex justify-content-center ">
        
@@ -126,39 +125,22 @@ function drawCard(icons){
             <h4>${icons.name}</h4>
     </div>
 </div>
-    `
+    `;
     rowHtml += cardTpl;
-})
-row.innerHTML += rowHtml;
+  });
+  row.innerHTML = rowHtml;
 }
-
-drawCard(icons)
-
-
-selectMenu.addEventListener('change', selectOption);
-
-function selectOption(){
-  const selectMenu = document.getElementById('selectMenu').value;
-
-    console.log(selectMenu);
-}
+drawCard(icons);
 
 const filterICon = [];
+selectMenu.addEventListener("change", selectOption);
 
-function drawIcon(selectOption){
-    const filterICon = icons.filter((value) =>{
+function selectOption() {
+  const selectMenu = document.getElementById("selectMenu").value;
 
-            if(selectMenu.value === 'all'){
-                drawCard();
-            } else {
-               
-            }
-        
-        })
-console.log(filterICon)
-    }
-  
+  const filterICon = icons.filter(
+    (icon) => icon.type == selectMenu || selectMenu == "all"
+  );
 
-
-
-
+  drawCard(filterICon);
+}
